f6b2e5b72a10bbed0ac9b8d73da035a3444f84e8
Nkaze Anderson <nkazeanderson@gmail.com>
2026-01-15 06:28:24 +0100

    Minor

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,3 +1,4 @@
+import SupabaseClientInit from "@/components/SupabaseClientInit";
 import UserContextProvider from "@/components/users/UserContext";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <SupabaseClientInit /> */}
+        <SupabaseClientInit />
         <UserContextProvider>
           <div className=" w-screen h-screen fixed text-gray-300">{children}</div>
         </UserContextProvider>
